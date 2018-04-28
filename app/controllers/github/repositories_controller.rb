229697ca893526fcad1192ff2c7e029c8bd5c15b
ota42y <ota42y@gmail.com>
2018-04-29 01:50:36 +0900

    :+1: register repository

--- app/controllers/github/repositories_controller.rb
+++ app/controllers/github/repositories_controller.rb
@@ -5,7 +5,8 @@ class Github::RepositoriesController < ApplicationController
 
   def create
     # TODO: check github user and repository access right
-    ::Github::User.find_or_create_by!(name: params[:user])
+    user = ::Github::User.find_or_create_by!(name: params[:user])
+    user.github_user_repositories.find_or_create_by!(repository: params[:repository])
 
     redirect_to '/github/users/ota42y/repositories/test'
   end
