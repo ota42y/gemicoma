9fd0b4c2a8ed101a012826a93b048418a003c3e1
ota42y <ota42y@gmail.com>
2018-04-30 10:20:00 +0900

    t

--- app/services/analyze_commit_service.rb
+++ app/services/analyze_commit_service.rb
@@ -5,10 +5,18 @@ class AnalyzeCommitService
       return false if commit.done? # other thread done
 
       ruby_gem_info = commit.github_repository.github_ruby_gem_info
-      ::V1::RubygemAnalyzer.new(commit).save! if ruby_gem_info
+      analyze_rubygem(commit) if ruby_gem_info
 
       commit.done!
       true
     end
+
+    private
+
+      # @param [Github::Commit] commit
+      def analyze_rubygem(commit)
+
+        ::V1::RubygemAnalyzer.new(commit).save!
+      end
   end
 end
