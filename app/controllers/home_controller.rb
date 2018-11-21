955794d5b9a07646f8e0f6a8fe5e660748b93109
ota42y <ota42y@gmail.com>
2018-11-21 12:39:12 +0900

    use revision latest

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,10 @@
 class HomeController < ApplicationController
   def index
-    repositories = ::Github::Repository.includes(:github_user, revisions: { revision_dependency_files: :revision_ruby_specifications }).all
 
     rubygem_specification = V1::RubygemsLoader.default_rubygems
-    # N+1... :(
-    # use Revision::Latest
-    revision_dict = repositories.map { |r| [r, r.revisions.sort_by(&:created_at).reverse.first] }.to_h
+
+    repositories = ::Revision::Latest.includes(repository: [:github_user, revisions: { revision_dependency_files: :revision_ruby_specifications }]).all.map(&:repository)
+    revision_dict = repositories.map { |r| [r, r.revisions.first] }.to_h
 
     dependency = revision_dict.map do |repo, r|
       [repo, ::V1::DependencyGraph.new(r.revision_dependency_files.first, rubygem_specification)]
